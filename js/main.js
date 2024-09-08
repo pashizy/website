01dc357af9a31abb0561e05ec9f13472a9f307a0
Pavlo Biliach <152802085+pashizy@users.noreply.github.com>
2024-09-08 23:10:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+}
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
